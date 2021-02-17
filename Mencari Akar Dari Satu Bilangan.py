e2ae6d3c600ed41d6df2289cad79b35a74a9aca9
gibrannnn <gibrangibran2303@gmail.com>
2021-02-17 10:18:49 +0700

    Gibran Ahmad Hidayat(14/XI-5)

---
+++ Mencari Akar Dari Satu Bilangan.py
@@ -0,0 +1,9 @@
+#import module
+import math
+
+#input number
+angka = 9
+
+#cari nilai akar
+akar_angka = maaath.sqrt(angka)
+print("akar dari"," angka"," adalah",akar_angka)
---
+++ Mencari Nilai Random.py
@@ -0,0 +1,5 @@
+#import modul
+import random
+
+print(random.radiant(0,9))
+print(random.radiant(1,10))
